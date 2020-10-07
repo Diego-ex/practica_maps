dd07a4e539eb59f8e0538d5282eb70e04784b6ba
Diego-ex <diego.pobletesanhueza@gmail.com>
2020-10-07 12:28:14 -0300

    Segundo Commit: Creación de activity de google maps.

--- app/src/main/java/com/poblete/practica_maps/MainActivity.java
+++ app/src/main/java/com/poblete/practica_maps/MainActivity.java
@@ -2,13 +2,47 @@ package com.poblete.practica_maps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText txtLatitud, txtLong, txtNombre;
+    Button btnMaps, btnLimpiar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtLatitud = findViewById(R.id.txtLatitud);
+        txtLong = findViewById(R.id.txtLong);
+        txtNombre = findViewById(R.id.txtNombre);
+        btnMaps = findViewById(R.id.btnMaps);
+        btnLimpiar = findViewById(R.id.btnLimpiar);
+
+        btnMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float lat ,lon;
+                String nombre;
+
+                lat = Float.parseFloat(txtLatitud.getText().toString());
+                lon = Float.parseFloat(txtLong.getText().toString());
+                nombre = txtNombre.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                intent.putExtra("Latitud", lat);
+                intent.putExtra("Longitud", lon);
+                intent.putExtra("Nombre", nombre);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
